45a3ca48095af70c2398f6c742630f1cd2fc3a24
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-15 01:13:27 +0530

    Added: add loadIndianState method to write a data from csv file

--- main/java/censusanalyser/IndiaStateCSV.java
+++ main/java/censusanalyser/IndiaStateCSV.java
@@ -5,20 +5,23 @@ import com.opencsv.bean.CsvBindByName;
 public class IndiaStateCSV
 {
     @CsvBindByName(column = "SrNo", required = true)
-    public int SrNo;
+    public int srNo;
+
     @CsvBindByName(column = "State Name", required = true)
     public String state;
+
     @CsvBindByName(column = "TIN", required = true)
-    public int TIN;
+    public int tin;
+
     @CsvBindByName(column = "StateCode", required = true)
     public String stateCode;
 
     @Override
     public String toString() {
         return "IndiaStateCSV{" +
-                "SrNo=" + SrNo +
+                "srNo=" + srNo +
                 ", state='" + state + '\'' +
-                ", TIN=" + TIN +
+                ", tin=" + tin +
                 ", stateCode='" + stateCode + '\'' +
                 '}';
     }
